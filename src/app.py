e60038120dab5a33e9ef9f5f35f225629be56464
vmahdych <vitalii.mahdych@gmail.com>
2023-01-22 23:37:51 +0200

    created photo uploader

--- src/app.py
+++ src/app.py
@@ -1,15 +1,13 @@
-from flask import Flask, jsonify, render_template
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask import Flask, jsonify, render_template, request, redirect, url_for, send_from_directory, flash
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 import socket
- 
+import os
 
 app = Flask(__name__)
-
-
-# Configure the upload set
-screenshots = UploadSet('screenshots', IMAGES)
-configure_uploads(app, (screenshots,))
-
+photos=UploadSet("photos", IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = './img'
+app.config["SECRET_KEY"] = os.urandom(24)
+configure_uploads(app, photos)
 
 # Function to fetch hostname and IP address
 def fetchDetails():
@@ -17,11 +15,9 @@ def fetchDetails():
     addr = socket.gethostbyname(hostname)
     return str(hostname), str(addr)
 
-
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
-
 
 @app.route("/health")
 def health():
@@ -29,30 +25,22 @@ def health():
         status="UP"
     )
 
-
 @app.route("/details")
 def details():
     hostname, addr = fetchDetails()
     return render_template('index.html', hostname=hostname, IP=addr)
 
-
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST' and 'screenshot' in request.files:
-        filename = screenshots.save(request.files['screenshot'])
-        return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <form method="post" enctype="multipart/form-data">
-        <input type="file" name="screenshot">
-        <input type="submit" value="Upload">
-    </form>
-    '''
-
+    if request.method == 'POST' and 'photo' in request.files:
+        photos.save(request.files['photo'])
+        flash("Photo saved successfully.")
+        return render_template('upload.html')
+    return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOADED_SCREENSHOTS_DEST'], filename)
-
+    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
